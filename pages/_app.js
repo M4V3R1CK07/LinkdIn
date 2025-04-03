b811f6b40e39bff2ef71f121f90fb090ee9c47f1
M4V3R1CK07 <aditya2708kumar@gmail.com>
2025-04-04 00:45:36 +0530

    fixed theme issues in homepage

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,17 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { RecoilRoot } from "recoil";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  // If the current route is "/" then force the theme to "light", otherwise allow normal theme switching.
+  const forcedTheme = router.pathname === "/" ? "light" : undefined;
+
   return (
     <SessionProvider session={pageProps.session}>
       <RecoilRoot>
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" forcedTheme={forcedTheme}>
           <Component {...pageProps} />
         </ThemeProvider>
       </RecoilRoot>
